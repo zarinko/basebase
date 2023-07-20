ad71beec69151ebc974db6923f1df04d7b90ff16
zarinko <96787748+zarinko@users.noreply.github.com>
2023-07-20 13:34:31 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x4b79ea9e16dc37f632304d0f25473fd67287ee37";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
